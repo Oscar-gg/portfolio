204bb465b8066c3f27bd73429a0d7c3f112fa0f6
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-12-31 14:04:02 -0600

    feat: descriptive messages for filter

--- src/components/List/projectList.tsx
+++ src/components/List/projectList.tsx
@@ -1,17 +1,26 @@
 import { ProjectCard, ProjectInfo } from "~/components/card/projectCard";
 
-export const ProjectList = ({ projects }: { projects: ProjectInfo[] }) => {
+export const ProjectList = ({ projects, hasFilters }: { projects: ProjectInfo[], hasFilters: boolean }) => {
+  const visibleProjects = projects.slice(0, 7);
+
   return (
-    <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
-      {projects.length > 0 ? (
-        projects.map((project) => (
-          <ProjectCard key={project.title} projectInfo={project} />
-        ))
-      ) : (
-        <p className="text-white">
-          No projects found with the specified filters.
+    <div>
+      {projects.length !== visibleProjects.length && (
+        <p className="text-white mb-4">
+          Showing {visibleProjects.length} of {projects.length} {hasFilters && "filtered"} projects.
         </p>
       )}
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
+        {visibleProjects.length > 0 ? (
+          visibleProjects.map((project) => (
+            <ProjectCard key={project.title} projectInfo={project} />
+          ))
+        ) : (
+          <p className="text-white">
+            No projects found with the specified filters.
+          </p>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,6 @@ import { Section } from "~/components/Layout/Section";
 import { projects } from "~/data/typed/objects";
 
 import { RefObject, useEffect, useRef, useState } from "react";
-import { ProjectCard } from "~/components/card/projectCard";
 import { ProjectList } from "./List/projectList";
 import { ProjectFilter } from "./filter/projectFilter";
 
@@ -18,20 +17,21 @@ export const Projects = () => {
     before: "",
   });
 
+  const hasFilters =
+    filterState.technologies.length > 0 ||
+    filterState.search !== "" ||
+    filterState.after !== "" ||
+    filterState.before !== "";
 
   return (
-    <Section title="Projects">
-      <p className="mb-5 text-justify text-white">
-        These are some of the projects I have worked on. You can click on them
-        to see more details.
-      </p>
+    <>
       <ProjectFilter
         projects={projects}
         setFilteredProjects={setFilteredProjects}
         filterState={filterState}
         setFilterState={setFilterState}
       />
-      <ProjectList projects={filteredProjects} />
-    </Section>
+      <ProjectList projects={filteredProjects} hasFilters={hasFilters} />
+    </>
   );
 };
