49e47a12352910bdac701075ec0cf59ace02a032
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-01-06 17:44:57 -0600

    info: added technologies to select

---
+++ src/data/utils/technologies.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+# Get list of technologies from Projects.json
+
+projects_path = os.path.join(os.getcwd(), './src/data/json/Projects.json')
+
+with open(projects_path) as f:
+    data = json.load(f)
+    projects = data['projects']
+    technologies = []
+
+    for project in projects:
+        for technology in project['technologies']:
+            if technology not in technologies:
+                technologies.append(technology)
+
+    technologies = sorted(technologies)
+
+    result = "technologiesOptions:["
+    for technology in technologies:
+        result += f'"{technology}",'
+    result = result[:-1] + "]"
+
+    print(result)
+    
